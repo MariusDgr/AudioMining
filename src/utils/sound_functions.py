530814cd4bd9e5088d27b493d7b9dafa869956c9
MariusDgr <32437161+MariusDgr@users.noreply.github.com>
2020-01-28 00:57:38 +0200

    tried ica

--- src/utils/sound_functions.py
+++ src/utils/sound_functions.py
@@ -3,8 +3,48 @@ import pydub
 import os
 from scipy.io import wavfile
 
+
+#################################### Data handling functions
+# as seen in https://stackoverflow.com/questions/53633177/how-to-read-a-mp3-audio-file-into-a-numpy-array-save-a-numpy-array-to-mp3?noredirect=1&lq=1
+def read_mp3(f, normalized=False):
+    """MP3 to numpy array"""
+    a = pydub.AudioSegment.from_mp3(f)
+    y = np.array(a.get_array_of_samples())
+    if a.channels == 2:
+        y = y.reshape((-1, 2))
+    if normalized:
+        return a.frame_rate, np.float32(y) / 2**15
+    else:
+        return a.frame_rate, y
+
+def write_mp3(f, sr, x, normalized=False):
+    """numpy array to MP3"""
+    channels = 2 if (x.ndim == 2 and x.shape[1] == 2) else 1
+    if normalized:  # normalized array - each item should be a float in [-1, 1)
+        y = np.int16(x * 2 ** 15)
+    else:
+        y = np.int16(x)
+    song = pydub.AudioSegment(y.tobytes(), frame_rate=sr, sample_width=2, channels=channels)
+    song.export(f, format="mp3", bitrate="320k")
+
+def read_all_files_as_numpy():
+    pass
+
+
+####################### Signal processing functions ##################
 def range_map(x, inp_range, out_range):
     return (x - inp_range[0]) * (out_range[1] - out_range[0]) / (inp_range[1] - inp_range[0]) + out_range[0]
+
+def pad_length_to_max(soundList, pad_value):
+    lens = [len(x) for x in soundList]
+    maxLen = pad_value
+    paddedArrays = []
+    for vect in soundList:
+        pad_len = maxLen - len(vect)
+        padding = np.zeros((1, pad_len))[0].astype(int)
+        paddedArrays.append(np.concatenate((vect, padding)))
+
+    return paddedArrays
 
 
 def mix_samples(sample1, sample2, r1=0.5, r2=0.5):
@@ -30,36 +70,3 @@ def mix_samples(sample1, sample2, r1=0.5, r2=0.5):
         sample2 = np.concatenate((sample2, padding))
 
     return r1 * sample1 + r2 * sample2
-
-# as seen in https://stackoverflow.com/questions/53633177/how-to-read-a-mp3-audio-file-into-a-numpy-array-save-a-numpy-array-to-mp3?noredirect=1&lq=1
-def read_mp3(f, normalized=False):
-    """MP3 to numpy array"""
-    a = pydub.AudioSegment.from_mp3(f)
-    y = np.array(a.get_array_of_samples())
-    if a.channels == 2:
-        y = y.reshape((-1, 2))
-    if normalized:
-        return a.frame_rate, np.float32(y) / 2**15
-    else:
-        return a.frame_rate, y
-
-def write_mp3(f, sr, x, normalized=False):
-    """numpy array to MP3"""
-    channels = 2 if (x.ndim == 2 and x.shape[1] == 2) else 1
-    if normalized:  # normalized array - each item should be a float in [-1, 1)
-        y = np.int16(x * 2 ** 15)
-    else:
-        y = np.int16(x)
-    song = pydub.AudioSegment(y.tobytes(), frame_rate=sr, sample_width=2, channels=channels)
-    song.export(f, format="mp3", bitrate="320k")
-
-def pad_length_to_max(soundList):
-    lens = [len(x) for x in soundList]
-    maxLen = max(lens)
-    paddedArrays = []
-    for vect in soundList:
-        pad_len = maxLen - len(vect)
-        padding = np.zeros((1, pad_len))[0].astype(int)
-        paddedArrays.append(np.concatenate((vect, padding)))
-
-    return paddedArrays
